a62559ecafeae459dea4a577d1c58391af7e4e52
Gawo <kevgawo@gmail.com>
2017-09-30 21:52:21 +0000

    this a feature branch for the user profile

---
+++ User_Education1.py
@@ -0,0 +1,34 @@
+#Linkedin project
+#A user Education, a user must have a school nmae
+#A user must have alevel of education in school
+#a user must have a course being studied
+#a user must have the year they started school
+#a user must have a year they completed school
+#A  school must have a Url
+
+
+
+class User_Eduction:
+
+    def __int__(self):
+
+        pass
+
+    def school_name(self):
+        self.schoolname  = input("Enter the school you went to: ")
+        return self.schoolname
+
+    def edu_level(self):
+        self.
+
+    def major_course(self):
+        pass
+
+    def start_year(self):
+        pass
+
+    def school_url(self):
+        pass
+
+education_history = User_Eduction()
+print(education_history.school_name())
