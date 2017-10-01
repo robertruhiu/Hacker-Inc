d901189629c7e8aea65ddd51d602d3e838781c95
Kobina Koomson <georgeranch31@gmail.com>
2017-10-01 21:12:33 +0000

    created the initial content for the user class

---
+++ User.py
@@ -0,0 +1,11 @@
+class User :
+    def __init__(self) :
+        self.__id = None
+        self.__firs_tname = None
+        self.__last_name = None
+        self.__email = None
+        self.__gender = None
+        self.__password = None
+        self.__date_registered = None
+        self.__dob = None
+        self.__educational_background = None
