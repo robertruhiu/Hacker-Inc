a65dd24574b9dafc02d09de515630f469adae74c
Kobina Koomson <georgeranch31@gmail.com>
2017-10-01 23:06:07 +0000

    updated the user class

--- User.py
+++ User.py
@@ -1,6 +1,8 @@
 class User :
     def __init__(self) :
-        self.__id = None
+        self.__id = None # set by the db engine if data's gon be saved in a db
+                         # else we'll have to make it a static field and increament
+                         #  it anytime an object is created out of this class
         self.__firs_tname = None
         self.__last_name = None
         self.__email = None
@@ -8,4 +10,28 @@ class User :
         self.__password = None
         self.__date_registered = None
         self.__dob = None
-        self.__educational_background = None
+        self.__educational_background = None #will contain an object of the ed_bg class
+
+        self.__headline = None
+        self.__country = None
+        self.__zipcode = None
+        self.__profile_Img = None
+        self.__summary = None
+        self.__links = [] # a list of urls
+        self.__skills = None
+        self.__of = None
+        self.__string = None
+        self.__interest = [] # a list of interests<String>
+
+        self.__followers = [] # a list of user ids
+        self.__following = [] # a list of user ids
+        self.__connections = [] # a list of user ids
+
+        self.__posts = [] # a list of post ids
+
+
+    def create(self):
+        pass
+
+    def delete_user(self, user_id):
+        pass
