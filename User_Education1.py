aaad53a2d82903ba677deda58db2e57c04c7bdf4
Gawo <kevgawo@gmail.com>
2017-10-02 16:42:50 +0000

    updated user education

--- User_Education1.py
+++ User_Education1.py
@@ -1,34 +1,78 @@
-#Linkedin project
-#A user Education, a user must have a school nmae
-#A user must have alevel of education in school
-#a user must have a course being studied
-#a user must have the year they started school
-#a user must have a year they completed school
-#A  school must have a Url
+class School:
+    def __init__(self, name, year_completed=None, course_studied="", year_started=None, school_url=" "):
+        """
+        :param name:
+        :param year_completed:
+        :param course_studied:
+        :param year_started:
+        :param school_url:
+        :return:
+        """
+        self.__name = name
+        self.__year_completed = year_completed
+        self.__year_started = year_started
+        self.__course_studied = course_studied
+
+    def get_name(self):
+        return self.__name
 
 
+class User_Education:
+    def __init__(self):
+        self.__schools = []
 
-class User_Eduction:
+    def school_exists(self, school_name):
+        """
+        :param school_name:
+        :return:
+        """
+        schools = self.__schools
+        for school in schools:
 
-    def __int__(self):
+            if school.get_name() == school:
+                return school
+        return False
 
-        pass
+    def add_school(self, school):
+        # FIXME implement this well
+        self.__schools.append(school)
 
-    def school_name(self):
-        self.schoolname  = input("Enter the school you went to: ")
-        return self.schoolname
+    def is_schools_list_empty(self):
+        """
+        :param self:
+        :param type: string
+        :return:
+        """
+        return (len(self.__schools) == 0)
 
-    def edu_level(self):
-        self.
+    def remove_school(self, school_name):
+        """
+        :param user_id:
+        :return:
+        """
+        schools = self.__schools
+        if self.is_schools_list_empty():
+            return "cannot remove from an empty list"
+        else:
+            school = self.school_exists(school_name)
 
-    def major_course(self):
-        pass
+            if school:
+                school.remove(school)
+                return school
+            else:
+                return "school not found"
 
-    def start_year(self):
-        pass
+    def find_school(self, school):
+        """
+        :param user_id:
+        :return:
+        """
+        school = self.school_exists(school)
 
-    def school_url(self):
-        pass
+        return "user not found" if school == False else school
 
-education_history = User_Eduction()
-print(education_history.school_name())
+
+user_education = User_Education()
+
+print(user_education.add_school(School("ana")))
+# print (user_education.add_school(input("Enter School name: ")))
