c98b9c9da9205f29c50126ae18296fd04d54cec5
pedmindset <emmarthurson@gmail.com>
2017-10-02 17:07:17 +0000

    post methods

--- post.py
+++ post.py
@@ -20,13 +20,13 @@ class Post:
         self.comments.append(c)
 
 # TEST: Uncomment the block of code below to test the class above and associated methods
-# my_post = Post()
-# while True:
-#     what_to_do = input('like/unlike/comment l/u/c: ')
-#     if what_to_do == 'l' or what_to_do == 'like':
-#         my_post.like()
-#     elif what_to_do == 'u' or what_to_do == 'unlike':
-#         my_post.unlike()
-#     elif what_to_do == 'c' or what_to_do == 'comment':
-#         my_post.comment()
-#     print(my_post.post, 'likes: {} * comments: {}'.format(my_post.likes, my_post.comments))
+my_post = Post()
+while True:
+    what_to_do = input('like/unlike/comment l/u/c: ')
+    if what_to_do == 'l' or what_to_do == 'like':
+        my_post.like()
+    elif what_to_do == 'u' or what_to_do == 'unlike':
+        my_post.unlike()
+    elif what_to_do == 'c' or what_to_do == 'comment':
+        my_post.comment()
+    print(my_post.post, 'likes: {} * comments: {}'.format(my_post.likes, my_post.comments))
