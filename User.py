cb406e6da49ea79a79b87793f4a81b201c6bb629
simon-suuk <simon.suuk@gmail.com>
2017-10-03 11:15:36 +0000

    modified implementations details for "add_follower" and "remove_follower" method

--- User.py
+++ User.py
@@ -2,30 +2,86 @@ class User:
     def __init__(self):
         self.__followers = []
 
+    def add_follower_id(self, id_of_follower):
+        if id_of_follower not in self.__followers:
+            self.__followers.append(id_of_follower)
+            return True
+        else:
+            return False
+
+    def remove_follower_id(self, id_of_follower):
+            self.__followers.remove(id_of_follower)
+
+    def get_all_followers_ids(self):
+        """
+        :return:
+        """
+        return self.__followers
+
 
 class UserManager:
     def __init__(self):
         self.__users = []
 
-    def add_follower(self, follower):
+    def person_exists(self, user_id):
         """
-        Adds a follower(user object) to a list
+        :param user_id:
+        :return:
+        """
+        people = self.__users
+        for person in people:
 
-        :rtype: object
+            if (person.get_id() == user_id):
+                return person
+        return False
+
+    def add_follower(self, user_id, id_of_follower):
+        """
+        Adds a follower to a user
+
+        :param id_of_follower:
+        :param user_id:
+        :rtype:
         :param follower: a user object
         """
-        self.__followers.append(follower)
+        if (user_id is None) or (id_of_follower is None):
+            return "please supply \"user_id\" and or \"id_of_follower\""
 
-    def remove_follower(self, id_of_follower):
+        temp_user = self.find_user_by_id(user_id)
+
+        if temp_user != "user not found":
+            user = User(temp_user)
+            temp_user = None
+            user.add_follower_id(id_of_follower)
+        else:
+            return "follower not added because user does not exist"
+
+    def remove_follower(self, user_id, id_of_follower):
         """
         Removes a follower(user object) from list by id
 
+        :param user_id:
         :rtype: object
         :param id_of_follower: the name of the follower to remove
         :return: an object of the user if removed else None object
         """
-        for a_follower in self.__followers:
-            if id_of_follower == a_follower.id:
-                self.__followers.remove(a_follower)
-                return a_follower
-        return None
+        if (user_id is None) or (id_of_follower is None):
+            return "please supply \"user_id\" and or \"id_of_follower\""
+
+        temp_user = self.find_user_by_id(user_id)
+
+        if temp_user != "user not found":
+            if id_of_follower in User(temp_user).get_all_followers_ids():
+                User(temp_user).remove_follower_id(id_of_follower)
+                return True
+        else:
+            return "follower not added because user does not exist"
+
+    def find_user_by_id(self, user_id):
+        """
+        :param user_id:
+        :return:
+        """
+        person = self.person_exists(user_id)
+
+        return "user not found" if person == False else person
