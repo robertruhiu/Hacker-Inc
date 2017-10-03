a58a65d6f6db72fc76f3db20cfbd51f1193b7d38
Terrence <owusuterrence@gmail.com>
2017-10-03 10:01:11 +0100

    made all the inputs private

--- comment.py
+++ comment.py
@@ -1,22 +1,22 @@
 
 class Reply:
     def __init__(self, user, content):
-        self.user = user
-        self.content = content
+        self.__user = user
+        self.__content = content
 
 
 class Comment:
     def __init__(self, user, content):
-        self.user = user
-        self.content = content
-        self.likes = 0
-        self.replies = []
+        self.__user = user
+        self.__content = content
+        self.__likes = 0
+        self.__replies = []
     
     def add_like_to_comment(self):
-        self.likes += 1
+        self.__likes += 1
 
     def add_reply(self, reply):
-        self.replies.append(reply)
+        self.__replies.append(reply)
 
     def delete_reply(self, reply):
         for rep in self.replies:
